0b7aaacde4fa65e081e1a781230e99c1a76bb5e6
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-10-11 17:02:27 +0200

    feat!(redis): add configurable namespace to redis cache

--- redis/redis.go
+++ redis/redis.go
@@ -12,17 +12,18 @@ import (
 // redis server.
 type cache struct {
 	connection redis.Conn
+	namespace  string // prefix every key to avoid collision
 }
 
 // cacheKey modifies an httpcache key for use in redis. Specifically, it
 // prefixes keys to avoid collision with other data stored in redis.
-func cacheKey(key string) string {
-	return "rediscache:" + key
+func cacheKey(namespace string, key string) string {
+	return namespace + key
 }
 
 // Get returns the response corresponding to key if present.
 func (c cache) Get(ctx context.Context, key string) (resp []byte, ok bool) {
-	item, err := redis.Bytes(redis.DoContext(c.connection, ctx, "GET", cacheKey(key)))
+	item, err := redis.Bytes(redis.DoContext(c.connection, ctx, "GET", cacheKey(c.namespace, key)))
 	if err != nil {
 		return nil, false
 	}
@@ -31,15 +32,15 @@ func (c cache) Get(ctx context.Context, key string) (resp []byte, ok bool) {
 
 // Set saves a response to the cache as key.
 func (c cache) Set(ctx context.Context, key string, resp []byte) {
-	redis.DoContext(c.connection, ctx, "SET", cacheKey(key), resp)
+	redis.DoContext(c.connection, ctx, "SET", cacheKey(c.namespace, key), resp)
 }
 
 // Delete removes the response with key from the cache.
 func (c cache) Delete(ctx context.Context, key string) {
-	redis.DoContext(c.connection, ctx, "DEL", cacheKey(key))
+	redis.DoContext(c.connection, ctx, "DEL", cacheKey(c.namespace, key))
 }
 
 // NewWithClient returns a new Cache with the given redis connection.
-func NewWithClient(client redis.Conn) httpcache.Cache {
-	return cache{client}
+func NewWithClient(client redis.Conn, namespace string) httpcache.Cache {
+	return cache{client, namespace}
 }
