8153b94e9896b984c6e30851c02885a791dc5e57
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-10-11 15:55:51 +0200

    deps: upgrade `diskv` to v3

--- diskcache/diskcache_test.go
+++ diskcache/diskcache_test.go
@@ -1,19 +1,22 @@
 package diskcache
 
 import (
-	"io/ioutil"
 	"os"
 	"testing"
 
+	"github.com/peterbourgon/diskv/v3"
 	"pkg.lovergne.dev/httpcache/test"
 )
 
 func TestDiskCache(t *testing.T) {
-	tempDir, err := ioutil.TempDir("", "httpcache")
+	tempDir, err := os.MkdirTemp("", "httpcache")
 	if err != nil {
 		t.Fatalf("TempDir: %v", err)
 	}
 	defer os.RemoveAll(tempDir)
-
-	test.Cache(t, New(tempDir))
+	kv := diskv.New(diskv.Options{
+		BasePath:     tempDir,
+		CacheSizeMax: 100 * 1024 * 1024, // 100MB
+	})
+	test.Cache(t, NewWithDiskv(kv))
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.24.7
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20250403215159-8d39553ac7cf
 	github.com/gomodule/redigo v1.9.3
-	github.com/peterbourgon/diskv v2.0.1+incompatible
+	github.com/peterbourgon/diskv/v3 v3.0.1
 	github.com/syndtr/goleveldb v1.0.0
 )
 
