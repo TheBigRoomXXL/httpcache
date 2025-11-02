4fe13e06eacf78057fa960f6ae19858ebd857ae0
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-11-02 13:00:58 +0100

    test: fail tests rather than skipping them if there service is not available

--- memcache/memcache_test.go
+++ memcache/memcache_test.go
@@ -14,7 +14,7 @@ const testServer = "localhost:11211"
 func TestMemCache(t *testing.T) {
 	conn, err := net.Dial("tcp", testServer)
 	if err != nil {
-		t.Skipf("skipping test; no server running at %s", testServer)
+		t.Fatalf("no server running at %s", testServer)
 	}
 	conn.Write([]byte("flush_all\r\n")) // flush memcache
 	conn.Close()
