db8cb7f9685cb2b8a1c19786aef8f53729cb35de
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-11-08 09:37:53 +0100

    ci: update release.sh

--- release.sh
+++ release.sh
@@ -1,43 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -z "$1" ]; then
   echo "Missing version tag."
-  echo "USAGE: release.sh <VERSION>"
+  echo "USAGE: $0 <VERSION>"
   exit 1
 fi
 
-
 NEW_VERSION=$1
 
-echo $1
-echo $NEW_VERSION
+git tag core/"$NEW_VERSION"
+git tag diskv/"$NEW_VERSION"
+git tag memcache/"$NEW_VERSION"
+git tag otter/"$NEW_VERSION"
+git tag redis/"$NEW_VERSION"
+git tag storagetest/"$NEW_VERSION"
 
-git tag core/$NEW_VERSION
-git push origin core/$NEW_VERSION
-
-(
-    cd ./diskv 
-    go get pkg.lovergne.dev/httpcache/core@$NEW_VERSION
-)
-(
-    cd ./otter
-    go get pkg.lovergne.dev/httpcache/core@$NEW_VERSION
-)
-(
-    cd ./memcache
-    go get pkg.lovergne.dev/httpcache/core@$NEW_VERSION
-)
-(
-    cd ./redis
-    go get pkg.lovergne.dev/httpcache/core@$NEW_VERSION
-)
-
-go work sync
-git add .
-git commit -m "chore: bump httpcache/core version in cache implementation"
-
-git tag diskv/$NEW_VERSION
-git tag memcache/$NEW_VERSION
-git tag otter/$NEW_VERSION
-git tag redis/$NEW_VERSION
-git push origin core/$NEW_VERSION otter/$NEW_VERSION diskv/$NEW_VERSION memcache/$NEW_VERSION redis/$NEW_VERSION
+git push origin \
+  core/"$NEW_VERSION" \
+  otter/"$NEW_VERSION" \
+  diskv/"$NEW_VERSION" \
+  memcache/"$NEW_VERSION" \
+  redis/"$NEW_VERSION" \
+  storagetest/"$NEW_VERSION"
