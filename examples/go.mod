b72cc08bcbc9867824e71a4187d1f9c92602867a
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-11-11 14:48:43 +0100

    basic example

--- examples/go.mod
+++ examples/go.mod
@@ -2,4 +2,4 @@ module pkg.lovergne.dev/httpcache/examples
 
 go 1.24.9
 
-require pkg.lovergne.dev/httpcache/core v0.1.0-beta11 // indirect
+require pkg.lovergne.dev/httpcache/core v0.1.0-beta11
--- examples/main.go
+++ examples/main.go
@@ -2,42 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"time"
-
-	httpcache "pkg.lovergne.dev/httpcache/core"
+	"os"
 )
 
+var examples = map[string]func(){
+	"basic": basic,
+}
+
 func main() {
-	storage := httpcache.NewInMemoryStorage()
-	clientWithCache := httpcache.NewCachedClient(storage)
-	url := "https://lovergne.dev"
-
-	start := time.Now()
-	resp, err := clientWithCache.Get(url)
-	if err != nil {
-		panic(err)
+	if len(os.Args) <= 1 {
+		fmt.Println("Bad Usage: you need to pass the example name.")
 	}
-	ellapsed := time.Since(start)
-
-	// ⚠ if you don't reach EOF on the body or close it, the request won't be cached.
-	io.ReadAll(resp.Body)
-	resp.Body.Close()
-
-	fmt.Println("First request:")
-	fmt.Println("  Cache-Control :", resp.Header.Get("Cache-Control"))
-	fmt.Println("  X-From-Cache :", resp.Header.Get("X-From-Cache"))
-	fmt.Println("  Ellapsed time: ", ellapsed)
-
-	start = time.Now()
-	resp, err = clientWithCache.Get(url)
-	if err != nil {
-		panic(err)
+	exampleFunc, ok := examples[os.Args[1]]
+	if !ok {
+		fmt.Println("Bad Usage: example not found.")
 	}
-	ellapsed = time.Since(start)
 
-	fmt.Println("\nSecond request:")
-	fmt.Println("  Cache-Control :", resp.Header.Get("Cache-Control"))
-	fmt.Println("  X-From-Cache :", resp.Header.Get("X-From-Cache"))
-	fmt.Println("  Ellapsed time: ", ellapsed)
+	exampleFunc()
 }
